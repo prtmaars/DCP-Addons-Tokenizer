8e3879916e92c036b477625b9458fd14fa0e6ba8
prtmaars <prtmaars@gmail.com>
2026-03-27 05:20:27 +0700

    Update tokenizer.py

--- tokenizer.py
+++ tokenizer.py
@@ -25,7 +25,7 @@ previous_year = "2026"
 @st.cache_data
 def load_data():
     try:
-        df = pd.read_csv('https://raw.github.com/prtmaars/DCP-Addons-Tokenizer/a8c0a5f2e7227d2ee4bec765cf422666f2e83e5f/tokenizer.csv', 
+        df = pd.read_csv('https://raw.github.com/prtmaars/DCP-Addons-Tokenizer/7909e523c215fc9689b081747a6c958f060e30fb/tokenizer.csv', 
                          sep=',',  
                          dtype=str, 
                          na_filter=False)
